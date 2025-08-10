01c3b81b2b645b47f0f842e79e450e2f153f3c07
YK-ov <bubkaogo@gmail.com>
2025-08-10 12:43:21 +0200

    initial commit

---
+++ Calculator/src/main/java/app/CalculatorApplication.java
@@ -0,0 +1,20 @@
+package app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CalculatorApplication extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        launch();
+    }
+
+
+}
